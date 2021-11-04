541f519a2af46eb9c7fcafb7c874c770a1ab4465
Alex Borza <alexborza860@yahoo.com>
2021-11-04 16:40:15 +0200

    first commit

---
+++ build/CMakeFiles/TemaLab4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemaLab4.dir/src/lab4.cpp.o"
+  "TemaLab4.pdb"
+  "TemaLab4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemaLab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
